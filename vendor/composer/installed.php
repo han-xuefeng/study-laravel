937a26988a975ec867156099e1c666d7dad00ffb
hanxuefeng <hanxuefeng@netjoy.com>
2021-11-17 20:35:51 +0800

    init code

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'han-xuefeng/study-laravel',
+  ),
+  'versions' => 
+  array (
+    'han-xuefeng/study-laravel' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
